deea79e8e7c968a5b94a54f20ca5665d32a9ea30
williamervin7 <william7ervin@gmail.com>
2026-04-30 16:49:05 -0500

    Completed Random Forest training script and main pipeline integration

--- main.py
+++ main.py
@@ -106,11 +106,22 @@ def go(config: DictConfig):
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            component_path = os.path.abspath(os.path.join("src", "train_random_forest"))
+            
+            _ = mlflow.run(
+                uri=component_path,
+                entry_point="main",
+                env_manager="conda",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": "random_forest_export"
+                },
+            )
 
         if "test_regression_model" in active_steps:
 
