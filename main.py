cd8d7f86fcdb8e675ed61f57ebb2467b69071cdd
williamervin7 <william7ervin@gmail.com>
2026-05-01 17:11:11 -0500

    Finalized pipeline added model testing

--- main.py
+++ main.py
@@ -125,11 +125,17 @@ def go(config: DictConfig):
 
         if "test_regression_model" in active_steps:
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            component_path = os.path.abspath(os.path.join(config["main"]["components_repository"], "test_regression_model"))
+            
+            _ = mlflow.run(
+                uri=component_path,
+                entry_point="main",
+                env_manager="conda",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+                },
+            )
 
 
 if __name__ == "__main__":
